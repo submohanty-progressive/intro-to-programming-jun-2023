93b412215852bdf59c4c0285180142f8041fdb6d
Sub Mohanty <subhankar_mohanty@progressive.com>
2023-06-23 14:03:42 -0400

    Start of post lunch

--- GreeterPractice/Greeter/GreetingMaker.cs
+++ GreeterPractice/Greeter/GreetingMaker.cs
@@ -1,4 +1,6 @@
-﻿namespace Greeter
+﻿using System.Reflection.Metadata;
+
+namespace Greeter
 {
     public class GreetingMaker
     {
@@ -6,9 +8,71 @@
         {
         }
 
-        public string Greet(string name)
+        bool IsAllUpper(string input)
         {
-            return "Hello, Windom.";
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (!Char.IsUpper(input[i]))
+                    return false;
+            }
+
+            return true;
+        }
+
+        public string Greet(params string[] Params) // requirement 5 - arbritrary list of names
+        {
+            string builder = "";
+            for (int i = 0; i < Params.Length - 1; i++)
+            {
+                builder += Params[i] + ", ";
+            }
+
+            bool checkExit = true;
+            string[] reorder = new string[Params.Length];
+            foreach (string param in Params)
+            {
+                if(IsAllUpper(param) == true)
+                {
+
+                }
+            }
+
+            // for requirement 6 part 1
+            bool checker = true;
+            foreach (string param in Params)
+            {
+                if(IsAllUpper(param) == true)
+                {
+                    checker = true;
+                }
+            }
+            
+            
+
+            if (checker == true) // requirement 6 - if all names are uppercase then shout the whole thing
+            {
+                return "HELLOm " + builder.ToUpper() + "!";
+            }
+
+            return "Hello, " + builder + "!";
+        }
+
+        public string Greet(string name1, string name2) // requirement 4 - two names
+        {
+            return "Hello, " + name1 + " and " + name2 + "!";
+        }
+
+        public string Greet(string name) 
+        {
+            if (IsAllUpper(name) == true) // requirement 3 - handle shouting
+            {
+                return "HELLO, " + name + "!";
+            }
+            if (name == null)
+            {       
+                return "Hello, chief!"; // requirement 2 - dealing with nulls
+            }
+            return "Hello, " + name + "."; // requirement 1 - saying hello
         }
     }
 }
